2029e4d8ab29ccff9da62ffc57092ed0b1dc81f7
nicolas <nicolas@damiens.info>
2013-12-06 23:11:07 +0100

    choix 32/64bits

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 if [[ ! -d nodejs ]]; then
-	wget http://nodejs.org/dist/v0.10.22/node-v0.10.22-linux-x64.tar.gz
-	tar zxf node-v0.10.22-linux-x64.tar.gz
-	mv node-v0.10.22-linux-x64 nodejs
+	n="x86"
+	m=`uname -m`
+	if [[ "$m" == "x86_64" ]]; then
+		n="x64"
+	fi
+	wget http://nodejs.org/dist/v0.10.22/node-v0.10.22-linux-$n.tar.gz
+	tar zxf node-v0.10.22-linux-$n.tar.gz
+	mv node-v0.10.22-linux-$n nodejs
 fi
 
 PATH=$PWD/nodejs/bin/:$PATH
